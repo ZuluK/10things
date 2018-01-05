c78cbf53b4d9c18b12c1a51ed7a0a5ec3d0cc6f9
Richard Spence <richardspence@Richards-MacBook-Air-2.local>
2018-01-04 20:21:41 -0800

    First Commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/* ---------------- Ten Things to master with Javascript ---------------------- */
+//Microsoft terms Javascript "J Script"
+//Also called ECMAScript
+//The language fo the web
+
+
+//How does Javascript work?
+wrapped in script tags in an external style sheet
+
+//How to test Javascript?
+Use web sources like plunker, jsfiddle or any other 3rd party web source.
+
+//How to use chrome to your advantage
+Using the console on your browser or inspect element to view.
+Can you 3rd party libraries and debug.
+
+//What is JSON?
+Javascript object notation
+not a programming languages just a way of writing JS
+
+//What AJAX
+the ability to update page dynamically without having to refresh the page. AJAX makes calls to server and updates page.
+
+
+//What to find answers to your questions?
+slackoverflow is great source.
+
+//Who invented JS?
+Brendan Eich former CEO of mozilla.
+
+//What is a JS Library?
+JS code thats written a certain way. Use less code.
+AngularJS, React are examples.
+
+//jQuery is still the most important library
